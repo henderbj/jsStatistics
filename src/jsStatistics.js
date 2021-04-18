7bf5fa6270283653b5bc94ad436904c6d5114804
Henderb Rodriguez <henderb.rodriguez.hr@gmail.com>
2021-04-18 17:37:57 +0000

    Added eslint and npm scripts

--- src/jsStatistics.js
+++ src/jsStatistics.js
@@ -15,66 +15,66 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const testArray = function(data){
-	if(!Array.isArray(data)){
-		throw new Error('data must be an array');
-	}
-}
+  if(!Array.isArray(data)){
+    throw new Error('data must be an array');
+  }
+};
 
 exports.average = function(data){
-	testArray(data);
-	return exports.sumAll(data) / data.length;
-}
+  testArray(data);
+  return exports.sumAll(data) / data.length;
+};
 
 exports.sumAll = function(data){
-	testArray(data);
-	return data.reduce(exports.sum2);
-}
+  testArray(data);
+  return data.reduce(exports.sum2);
+};
 
 exports.parseNumber = function(data) {
-	const parsed = parseFloat(data)
-	if (isNaN(parsed)) {
-		throw new Error('Tried to convert a Not-A-Number (NaN) to Float');
-	}
-	return parsed;
-}
+  const parsed = parseFloat(data);
+  if (isNaN(parsed)) {
+    throw new Error('Tried to convert a Not-A-Number (NaN) to Float');
+  }
+  return parsed;
+};
 
 exports.sum2 = (accumulator, currentValue) => {
-	return parseFloat(accumulator) + exports.parseNumber(currentValue);
-}
+  return parseFloat(accumulator) + exports.parseNumber(currentValue);
+};
 
 // get array with gains where currentItem > (currentItem-1)
 exports.gains = (data) => {
-	const result = data.map((current, index, array) => {
-		if(index === 0)
-			return null;
-		if(current > array[index-1]){
-			return parseFloat(current) - parseFloat(array[index-1]);
-		}
-		else return 0;
-	});
+  const result = data.map((current, index, array) => {
+    if(index === 0)
+      return null;
+    if(current > array[index-1]){
+      return parseFloat(current) - parseFloat(array[index-1]);
+    }
+    else return 0;
+  });
   return result.slice(1); //first element removed
-}
+};
 
 // get array with values where currentItem < (currentItem-1)
 exports.losses = (data) => {
-	const result = data.map((current, index, array) => {
-		if(index === 0)
-			return null;
-		if(current < array[index-1]){
-			return  parseFloat(array[index-1]) - parseFloat(current);
-		}
-		else return 0;
-	});
+  const result = data.map((current, index, array) => {
+    if(index === 0)
+      return null;
+    if(current < array[index-1]){
+      return  parseFloat(array[index-1]) - parseFloat(current);
+    }
+    else return 0;
+  });
   return result.slice(1); //first element removed
-}
+};
 
 // calculate RSI indicator of all values of data: https://www.macroption.com/rsi-calculation/
 exports.rsi = (data) => {
-	const ups = exports.gains(data);
-	const downs = exports.losses(data);
-	const avgUps = exports.average(ups);
-	const avgDowns = exports.average(downs);
-	const rs = avgUps / avgDowns;
-	const rsi = 100 - (100 / (rs + 1));
-	return rsi;
-}
+  const ups = exports.gains(data);
+  const downs = exports.losses(data);
+  const avgUps = exports.average(ups);
+  const avgDowns = exports.average(downs);
+  const rs = avgUps / avgDowns;
+  const rsi = 100 - (100 / (rs + 1));
+  return rsi;
+};
--- test/test.js
+++ test/test.js
@@ -15,58 +15,58 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const assert = require('assert');
-const stats = require('../../jsStatistics');
+const stats = require('../src/jsStatistics');
 
 describe('jsStatistics Module', function() {
-	describe('#average)', function() {
-		it('should throw error because data is no array', function() {
-			assert.throws(()=>{stats.average('')}, {
-				name: 'Error',
-				message: 'data must be an array'
-			});
-		});
+  describe('#average)', function() {
+    it('should throw error because data is no array', function() {
+      assert.throws(()=>{stats.average('');}, {
+        name: 'Error',
+        message: 'data must be an array'
+      });
+    });
 
-		it('should give correct average of array data', function() {
-			assert.equal(stats.average([1,2,3,4]), 2.5);
-		});
-	});
+    it('should give correct average of array data', function() {
+      assert.equal(stats.average([1,2,3,4]), 2.5);
+    });
+  });
 
-	describe('#sumAll)', function() {
-		it('should give correct sum of array data', function() {
-			assert.equal(stats.sumAll([1,2,3,4]), 10);
-		});   
-	});
+  describe('#sumAll)', function() {
+    it('should give correct sum of array data', function() {
+      assert.equal(stats.sumAll([1,2,3,4]), 10);
+    });   
+  });
 
-	describe('#parseNumber)', function() {
-		it('should throw error because data is no number', function() {
-			assert.throws(()=>{stats.parseNumber('Anything')}, {
-				name: 'Error',
-				message: 'Tried to convert a Not-A-Number (NaN) to Float'
-			});
-		});
+  describe('#parseNumber)', function() {
+    it('should throw error because data is no number', function() {
+      assert.throws(()=>{stats.parseNumber('Anything');}, {
+        name: 'Error',
+        message: 'Tried to convert a Not-A-Number (NaN) to Float'
+      });
+    });
 
-		it('should give correct value of number string', function() {
-			assert.equal(stats.parseNumber("100.25"), 100.25);
-		});   
-	});
+    it('should give correct value of number string', function() {
+      assert.equal(stats.parseNumber('100.25'), 100.25);
+    });   
+  });
 
-	describe('#rsi)', function() {
-		it('should give correct array of gains', function() {
-			assert.deepEqual(stats.gains([1,3,7,7,2,11]), [2,4,0,0,9]);
-		});
+  describe('#rsi)', function() {
+    it('should give correct array of gains', function() {
+      assert.deepEqual(stats.gains([1,3,7,7,2,11]), [2,4,0,0,9]);
+    });
 
-		it('should give correct array of losses', function() {
-			assert.deepEqual(stats.losses([1,3,7,7,2,11]), [0,0,0,5,0]);
-		});
+    it('should give correct array of losses', function() {
+      assert.deepEqual(stats.losses([1,3,7,7,2,11]), [0,0,0,5,0]);
+    });
 
-		it('should give correct rsi indicator', function() {
-			assert.equal(stats.rsi([1,3,7,7,2,11]), 75);
-		});
+    it('should give correct rsi indicator', function() {
+      assert.equal(stats.rsi([1,3,7,7,2,11]), 75);
+    });
 
-		it('should give correct rsi indicator of more real data', function() {
-			const testdata = [44.34,44.09,44.15,43.61,44.33,44.83,45.10,45.42,45.84,46.08,45.89,
+    it('should give correct rsi indicator of more real data', function() {
+      const testdata = [44.34,44.09,44.15,43.61,44.33,44.83,45.10,45.42,45.84,46.08,45.89,
         46.03,45.61,46.28,46.28];
-			assert.equal(stats.rsi(testdata), 70.46413502109705);
-		});
-	});
+      assert.equal(stats.rsi(testdata), 70.46413502109705);
+    });
+  });
 });
